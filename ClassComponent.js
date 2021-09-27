7ec3d85336dde3d787ff269b120cebf5ac435809
MeKa <48958010+metinkantar@users.noreply.github.com>
2021-09-27 21:16:43 +0300

    Create ClassComponent.js

---
+++ ClassComponent.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class ClassComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arttir: 0
+    };
+  }
+  sayiArttir = () => {
+    this.setState({
+      arttir: this.state.arttir + 1
+    });
+  };
+
+  sayiAzalt = () => {
+    this.setState({
+      arttir: this.state.arttir - 1
+    });
+  };
+
+  render() {
+    return (
+      <di>
+        <h1>Class Component</h1>
+        <h3>(Sınıf Bileşeni)</h3>
+        <p>Counter : {this.state.arttir}</p>
+        <button onClick={() => this.sayiArttir()}>Arttır</button>
+        <button onClick={() => this.sayiAzalt()}>Azalt</button>
+      </di>
+    );
+  }
+}
+
+export default ClassComponent;
